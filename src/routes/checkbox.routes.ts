ddb6dee6359890d74a4c586d62865bcc1444f3a6
gracebir <grace.devolop@gmail.com>
2024-06-30 22:06:50 +0200

    user service

--- src/routes/checkbox.routes.ts
+++ src/routes/checkbox.routes.ts
@@ -1,15 +1,10 @@
 /** @format */
-import { Router } from "express";
-import { CheckboxController } from "../controllers/checkbox.controller";
+import { Router } from 'express';
+import { toggle, list } from '../controllers/checkbox.controller';
 
 const router = Router();
-const checkboxController = new CheckboxController();
 
-router.get("/", (req, res) => checkboxController.getAllCheckboxes(req, res));
-router.get("/range", (req, res) =>
-    checkboxController.getCheckboxesByRange(req, res)
-);
-router.get("/:id", (req, res) => checkboxController.getCheckboxById(req, res));
-router.put("/:id", (req, res) => checkboxController.updateCheckbox(req, res));
+router.put('/:checkboxId', toggle);
+router.get('/', list);
 
 export default router;
---
+++ src/routes/user.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { register, authenticate } from '../controllers/user.controller';
+
+const router = Router();
+
+router.post('/register', register);
+router.post('/signin', authenticate);
+
+export default router;
