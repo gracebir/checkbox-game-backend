97b007fdd62b63520449ec46385d64866bc32c4b
gracebir <grace.devolop@gmail.com>
2024-06-30 18:23:00 +0200

    set up the database and craete the checkbox game

---
+++ src/db/index.ts
@@ -0,0 +1,16 @@
+/** @format */
+
+import { Sequelize } from "sequelize-typescript";
+import { Checkbox } from "./models/CheckboxGame.model";
+
+const connection = new Sequelize({
+    dialect: "postgres",
+    host: "localhost",
+    username: "hustle",
+    password: "firewall",
+    database: "game_checkbox",
+    logging: false,
+    models: [Checkbox],
+});
+
+export default connection;
