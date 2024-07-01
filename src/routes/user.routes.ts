e47f74d8836f27fe34d31f602e83f397778a97ba
gracebir <grace.devolop@gmail.com>
2024-07-01 16:28:53 +0200

    track user router

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,7 +5,9 @@ import * as userController from "../controllers/user.controller";
 
 const router = express.Router();
 
+
 router.post("/register", userController.registerUser);
 router.post("/authenticate", userController.authenticateUser);
+router.get("/:userId/check-count", userController.getUserCheckCount);
 
 export default router;
